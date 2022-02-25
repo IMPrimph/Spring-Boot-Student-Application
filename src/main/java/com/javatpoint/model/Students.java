1a81cf4991c64a840fcf8bda2b7461782171b9b4
IMPrimph <vishnugajulapalli123@gmail.com>
2022-02-25 18:02:10 +0530

    add sql to project

--- src/main/java/com/javatpoint/model/Students.java
+++ src/main/java/com/javatpoint/model/Students.java
@@ -6,7 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
-@Table
+@Table(name="student")
 
 public class Students {
 @Id
---
+++ src/main/resources/db/migration/V1__Initial.sql
@@ -0,0 +1,3 @@
+CREATE TABLE students (ID INT AUTO_INCREMENT PRIMARY KEY, studentName VARCHAR(45));
+INSERT INTO students (ID, studentName) VALUES (1,  'Vishnu');
+
